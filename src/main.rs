d81f92f84e86e23e927d6ffb8b11014187c53b9b
ickk <git@ickk.io>
2023-10-15 11:18:40 +1100

    make raylib systems more idiomatic

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,56 @@
 use bevy_app::prelude::*;
 use bevy_ecs::prelude::*;
+use bevy_utils::prelude::*;
 use raylib::prelude::*;
 
-struct RaylibContext {
-    rl: RaylibHandle,
-    thread: RaylibThread,
-}
-
-#[derive(Resource, Debug)]
-struct Cursor {
-    x: f32,
-    y: f32,
-}
-
 fn main() {
-    let raylib_context = {
-        let (rl, thread) = raylib::init().size(640, 480).title("Hello, World").build();
-        RaylibContext { rl, thread }
-    };
-
     App::new()
-        .insert_non_send_resource(raylib_context)
-        .add_systems(Update, update_system)
+        .init_resource::<Cursor>()
+        .insert_resource(WindowConfig {
+            title: "Bevy + Raylib".to_owned(),
+            ..default()
+        })
+        .add_systems(PreUpdate, update_cursor)
+        .add_systems(PostUpdate, render)
         .set_runner(runner)
         .run();
 }
 
 fn runner(mut app: App) {
-    let raylib_context = &mut app
+    let window_config = app
         .world
-        .remove_non_send_resource::<RaylibContext>()
-        .expect("RaylibContext doesn't exist");
+        .remove_resource::<WindowConfig>()
+        .unwrap_or_default();
+    let raylib_context = {
+        let (rl, thread) = raylib::init()
+            .size(window_config.width, window_config.height)
+            .title(&window_config.title)
+            .build();
+        RaylibContext { rl, thread }
+    };
+    app.world.insert_non_send_resource(raylib_context);
 
-    while !raylib_context.rl.window_should_close() {
-        input_mapping(raylib_context, &mut app.world);
+    let should_close = |app: &App| {
+        app.world
+            .get_non_send_resource::<RaylibContext>()
+            .map_or(false, |context| context.rl.window_should_close())
+    };
 
+    while !should_close(&app) {
         app.update();
-
-        draw(raylib_context, &mut app.world);
     }
 }
 
-fn update_system() {
-    // simulation
-}
-
-fn input_mapping(context: &mut RaylibContext, world: &mut World) {
-    let cursor = {
-        let Vector2 { x, y } = context.rl.get_mouse_position();
+fn update_cursor(raylib_context: NonSend<RaylibContext>, mut cursor: ResMut<Cursor>) {
+    *cursor = {
+        let Vector2 { x, y } = raylib_context.rl.get_mouse_position();
         Cursor { x, y }
-    };
-    world.insert_resource(cursor);
+    }
 }
 
-fn draw(context: &mut RaylibContext, world: &mut World) {
-    let mut d = context.rl.begin_drawing(&context.thread);
+fn render(mut raylib: NonSendMut<RaylibContext>, cursor: Res<Cursor>) {
+    let mut d = raylib.begin_drawing();
     d.clear_background(Color::WHITE);
-    let cursor = world.resource::<Cursor>();
     d.draw_text(
         &format!("{}, {}", cursor.x, cursor.y),
         12,
@@ -64,4 +58,38 @@ fn draw(context: &mut RaylibContext, world: &mut World) {
         20,
         Color::BLACK,
     );
+}
+
+struct RaylibContext {
+    rl: RaylibHandle,
+    thread: RaylibThread,
+}
+
+impl RaylibContext {
+    pub fn begin_drawing(&mut self) -> RaylibDrawHandle {
+        self.rl.begin_drawing(&self.thread)
+    }
+}
+
+#[derive(Resource)]
+struct WindowConfig {
+    pub width: i32,
+    pub height: i32,
+    pub title: String,
+}
+
+impl Default for WindowConfig {
+    fn default() -> Self {
+        WindowConfig {
+            width: 640,
+            height: 480,
+            title: "App".to_owned(),
+        }
+    }
+}
+
+#[derive(Resource, Debug, Default)]
+struct Cursor {
+    x: f32,
+    y: f32,
 }
