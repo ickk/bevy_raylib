450660f919a9f38bab89ef1a1e455b54e5fc2933
ickk <git@ickk.io>
2023-10-15 11:26:37 +1100

    turn into library

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,17 @@
 use bevy_app::prelude::*;
 use bevy_ecs::prelude::*;
+use bevy_raylib::prelude::*;
 use bevy_utils::prelude::*;
-use raylib::prelude::*;
 
 fn main() {
     App::new()
-        .init_resource::<Cursor>()
+        .add_plugins(RaylibPlugin)
         .insert_resource(WindowConfig {
             title: "Bevy + Raylib".to_owned(),
             ..default()
         })
-        .add_systems(PreUpdate, update_cursor)
         .add_systems(PostUpdate, render)
-        .set_runner(runner)
         .run();
-}
-
-fn runner(mut app: App) {
-    let window_config = app
-        .world
-        .remove_resource::<WindowConfig>()
-        .unwrap_or_default();
-    let raylib_context = {
-        let (rl, thread) = raylib::init()
-            .size(window_config.width, window_config.height)
-            .title(&window_config.title)
-            .build();
-        RaylibContext { rl, thread }
-    };
-    app.world.insert_non_send_resource(raylib_context);
-
-    let should_close = |app: &App| {
-        app.world
-            .get_non_send_resource::<RaylibContext>()
-            .map_or(false, |context| context.rl.window_should_close())
-    };
-
-    while !should_close(&app) {
-        app.update();
-    }
-}
-
-fn update_cursor(raylib_context: NonSend<RaylibContext>, mut cursor: ResMut<Cursor>) {
-    *cursor = {
-        let Vector2 { x, y } = raylib_context.rl.get_mouse_position();
-        Cursor { x, y }
-    }
 }
 
 fn render(mut raylib: NonSendMut<RaylibContext>, cursor: Res<Cursor>) {
@@ -58,38 +24,4 @@ fn render(mut raylib: NonSendMut<RaylibContext>, cursor: Res<Cursor>) {
         20,
         Color::BLACK,
     );
-}
-
-struct RaylibContext {
-    rl: RaylibHandle,
-    thread: RaylibThread,
-}
-
-impl RaylibContext {
-    pub fn begin_drawing(&mut self) -> RaylibDrawHandle {
-        self.rl.begin_drawing(&self.thread)
-    }
-}
-
-#[derive(Resource)]
-struct WindowConfig {
-    pub width: i32,
-    pub height: i32,
-    pub title: String,
-}
-
-impl Default for WindowConfig {
-    fn default() -> Self {
-        WindowConfig {
-            width: 640,
-            height: 480,
-            title: "App".to_owned(),
-        }
-    }
-}
-
-#[derive(Resource, Debug, Default)]
-struct Cursor {
-    x: f32,
-    y: f32,
 }
